170097225257c99d2b4cf05307bd66c73e529aad
Martin Sogoloff <martinsogoloff@gmail.com>
2026-08-13 12:13:50 -0300

    Add files via upload

--- talleres/taller_0/ejercicios/t0_ej3/circuito_ej3_tb.sv
+++ talleres/taller_0/ejercicios/t0_ej3/circuito_ej3_tb.sv
@@ -33,8 +33,8 @@ module circuito_ej3_tb;
   always_comb begin
     case (value)
       2'b00:   exp = 1'b0;  // TODO: a=0, b=0 → y = ? (completar con 1'b0 si quiero que y valga 0, o 1'b1 si quiero que y valga 1)
-      2'b01:   exp = 1'b0;  // TODO: a=0, b=1 → y = ? (completar con 1'b0 si quiero que y valga 0, o 1'b1 si quiero que y valga 1)
-      2'b10:   exp = 1'b0;  // TODO: a=1, b=0 → y = ? (completar con 1'b0 si quiero que y valga 0, o 1'b1 si quiero que y valga 1)
+      2'b01:   exp = 1'b1;  // TODO: a=0, b=1 → y = ? (completar con 1'b0 si quiero que y valga 0, o 1'b1 si quiero que y valga 1)
+      2'b10:   exp = 1'b1;  // TODO: a=1, b=0 → y = ? (completar con 1'b0 si quiero que y valga 0, o 1'b1 si quiero que y valga 1)
       2'b11:   exp = 1'b0;  // TODO: a=1, b=1 → y = ? (completar con 1'b0 si quiero que y valga 0, o 1'b1 si quiero que y valga 1)
       default: exp = 1'b0;
     endcase
