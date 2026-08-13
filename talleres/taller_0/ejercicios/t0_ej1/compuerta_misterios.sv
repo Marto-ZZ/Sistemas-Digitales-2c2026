7fea78e342c346fedee7168ab8da23e59977c3c6
Martin Sogoloff <martinsogoloff@gmail.com>
2026-08-13 12:21:33 -0300

    Add files via upload

--- talleres/taller_0/ejercicios/t0_ej1/compuerta_misterios.sv
+++ talleres/taller_0/ejercicios/t0_ej1/compuerta_misterios.sv
@@ -2,10 +2,10 @@
 // Abrir, sintetizar y explorar.
 
 module compuerta_misterios (
-    input  logic a,
-    output logic y
+    input  logic a, // Agregamos una variable de entrada
+    output logic y // Agregamos una variable de salida
 );
 
-  assign y = ~a;
+  assign y = ~a; // Asignamos la relacion logica entre salida y entrada
 
 endmodule
