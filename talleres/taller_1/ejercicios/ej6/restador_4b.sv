6716ce060429c8106b9556c7a92687d5f37477cf
Marto-ZZ <martinsogoloff@gmail.com>
2026-08-20 13:49:01 -0300

    ej 6

--- talleres/taller_1/ejercicios/ej6/restador_4b.sv
+++ talleres/taller_1/ejercicios/ej6/restador_4b.sv
@@ -5,6 +5,33 @@ module restador_4b (
     output logic [3:0] diff,
     output logic       bout
 );
-  logic b1, b2, b3;
-  // instanciar cuatro restador_completo
+  logic b1, b2, b3; 
+  restador_completo r1(
+    .a(a[0]),
+    .b(b[0]),
+    .bin(bin),
+    .diff(diff[0]),
+    .bout(b1)
+  );
+  restador_completo r2(
+    .a(a[1]),
+    .b(b[1]),
+    .bin(b1),
+    .diff(diff[1]),
+    .bout(b2)
+  );
+  restador_completo r3(
+    .a(a[2]),
+    .b(b[2]),
+    .bin(b2),
+    .diff(diff[2]),
+    .bout(b3)
+  );
+    restador_completo r4(
+    .a(a[3]),
+    .b(b[3]),
+    .bin(b3),
+    .diff(diff[3]),
+    .bout(bout)
+  );
 endmodule
