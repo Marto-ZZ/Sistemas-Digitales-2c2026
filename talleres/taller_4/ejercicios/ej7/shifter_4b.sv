17fa29d0a2864db96fb9bb9a600aad3fede38c14
Marto-ZZ <martinsogoloff@gmail.com>
2026-09-13 18:50:51 -0300

    taller 4 update

--- talleres/taller_4/ejercicios/ej7/shifter_4b.sv
+++ talleres/taller_4/ejercicios/ej7/shifter_4b.sv
@@ -7,4 +7,20 @@ module shifter_4b (
   // COMPLETAR: desplazar dato una posición a derecha según aritmetico.
   // Reutilizar negativo_4b y zero_4b sobre resultado.
   // En modo lógico, ambos flags deben valer 0.
+  assign resultado = aritmetico ? {dato[3], dato[3:1]} : {1'b0, dato[3:1]};
+  logic flag_n;
+  logic flag_z;
+
+  negativo_4b negativo_4b (
+    .dato    (resultado),
+    .negativo(flag_n)
+  );
+
+  zero_4b zero_4b (
+    .dato(resultado),
+    .zero(flag_z)
+  );
+
+  assign negativo = aritmetico ? flag_n : 1'b0;
+  assign zero = aritmetico ? flag_z : 1'b0;
 endmodule
--- talleres/taller_4/ejercicios/ej7/zero_4b.sv
+++ talleres/taller_4/ejercicios/ej7/zero_4b.sv
@@ -2,5 +2,9 @@ module zero_4b (
     input logic [3:0] dato,
     output logic zero
 );
-  // COMPLETAR: Indicar si dato es cero.
+  assign zero = dato[3] == 0 &&
+                dato[2] == 0 &&
+                dato[1] == 0 &&
+                dato[0] == 0 ?
+                1 : 0;
 endmodule
